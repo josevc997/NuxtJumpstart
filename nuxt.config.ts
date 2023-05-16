d4463869f394bd5a749bf3fb47b4c2ad36af8435
Jose Valencia <19josevalencia97@gmail.com>
2023-05-16 08:10:45 -0400

    feat: install nuxt-content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+const path = require("path");
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: ["~/assets/css/main.css"],
@@ -7,9 +8,19 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
-    modules: ["nuxt-icon", "nuxt-headlessui"],
+    modules: ["nuxt-icon", "nuxt-headlessui", "@nuxt/content"],
     // Optionally change the default prefix.
     headlessui: {
         prefix: "",
+    },
+    content: {
+        sources: {
+            // overwrite default source AKA `content` directory
+            content: {
+                driver: "fs",
+                prefix: "/docs", // All contents inside this source will be prefixed with `/docs`
+                base: path.resolve(__dirname, "content"),
+            },
+        },
     },
 });
